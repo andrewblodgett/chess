292fe9044526d6950c68c49a5ab760011b3e11a5
Andrew Blodgett <andre276@byu.edu>
2025-10-11 15:48:49 -0600

    added faker methods on the server to test endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -14,6 +14,11 @@ public class Server {
         server = Javalin.create(config -> config.staticFiles.add("web"));
         server.delete("db", ctx -> ctx.result("{}"));
         server.post("user", this::register);
+        server.post("session", this::login);
+        server.delete("session", this::logout);
+        server.post("game", this::createGame);
+        server.put("game", this::joinGame);
+        server.get("game", this::listGames);
         // Register your endpoints and exception handlers here.
 
     }
@@ -25,6 +30,34 @@ public class Server {
         request.put("authToken", "yeet");
         var response = serializer.toJson(request);
         ctx.result(response);
+    }
+
+    private void login(Context ctx) {
+        var serializer = new Gson();
+        var request = serializer.fromJson(ctx.body(), Map.class);
+        request.put("authToken", "yeet");
+        var response = serializer.toJson(request);
+        ctx.result(response);
+    }
+
+    private void logout(Context ctx){
+        var serializer = new Gson();
+//        var request = serializer.fromJson(ctx.body(), Map.class);
+//        request.put("authToken", "yeet");
+//        var response = serializer.toJson();
+        ctx.result("{\"authToken\":\"yeet\"}");
+    }
+
+    private void createGame(Context ctx) {
+        ctx.result("{\"gameID\":1234}");
+    }
+
+    private void joinGame(Context ctx) {
+        ctx.result("{}");
+    }
+
+    private void listGames(Context ctx) {
+        ctx.result("{\"games\":[]}");
     }
 
     public int run(int desiredPort) {
