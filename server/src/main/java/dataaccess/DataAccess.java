d1c54210a11932cf31c129ff9479fafd07bb77f6
Andrew Blodgett <andre276@byu.edu>
2025-10-17 11:27:21 -0600

    DataAccess interface

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,38 @@
+package dataaccess;
+
+/*
+clear: A method for clearing all data from the database. This is used during testing.
+createUser: Create a new user.
+getUser: Retrieve a user with the given username.
+createGame: Create a new game.
+getGame: Retrieve a specified game with the given game ID.
+listGames: Retrieve all games.
+updateGame: Updates a chess game. It should replace the chess game string corresponding to a given gameID. This is used when players join a game or when a move is made.
+createAuth: Create a new authorization.
+getAuth: Retrieve an authorization given an authToken.
+deleteAuth: Delete an authorization so that it is no longer valid
+ */
+
+
+import datamodel.AuthData;
+import datamodel.GameData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+    void clear();
+
+    void createUser();
+
+    void createGame();
+
+    GameData getGame(int gameID);
+
+    Collection<GameData> listGames();
+
+    void updateGame(int gameID);
+
+    AuthData createAuth();
+
+    void deleteAuth();
+}
